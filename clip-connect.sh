05fa137e6d4663885fa4b8120e6bd93e270c5710
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2021-09-21 14:57:18 +0200

    New install procedure

--- clip-connect.sh
+++ clip-connect.sh
@@ -3,36 +3,44 @@
 #
 # openconnect wrapper
 #
-# Dietrich Liko
+# Dietrich Liko, September 2021
 
 set -e
 
-# openconnect prefers SIGINT
+_die() {
+   echo "$@" 1>&2
+   exit 1
+}
 
+# openconnect prefers SIGINT
 _term() {
   kill -INT "$pid" 2>/dev/null
 }
 trap _term SIGTERM
 
-output=$(security find-generic-password -s clip-connect "${HOME}/Library/Keychains/login.keychain-db")
-USERNAME=$(echo "$output" | awk '/"acct"/ { print substr($1,15,length($1)-15)}')
+USERNAME=$(security find-generic-password -s clip-connect "${HOME}/Library/Keychains/login.keychain-db" | awk '/"acct"/ { print substr($1,15,length($1)-15)}' )
 PASSWORD=$(security find-generic-password -s clip-connect -w "${HOME}/Library/Keychains/login.keychain-db")
 
-OPENCONNECT='/opt/local/sbin/openconnect'
-
-VPN_SLICE='vpn-slice 172.16.0.0/16 cbe.vbc.ac.at jupyterhub.vbc.ac.at docs.vbc.ac.at'
-. /opt/clip-connect/venv/bin/activate
-export LC_ALL="en_US.UTF-8"
+for path in "/opt/local/sbin/openconnect" "/usr/local/bin/openconnect"
+do
+    [ -e $path ] && OPENCONNECT="$path"
+done
+[ -z $OPENCONNECT ] || _die "No openconnect binary found"
 
 VPN_URL="https://vpn.vbc.ac.at/AllSecure"
+VPN_SLICE="vpn-slice 172.16.0.0/16 cbe.vbc.ac.at jupyterhub.vbc.ac.at docs.vbc.ac.at"
 
-echo $PASSWORD | sudo $OPENCONNECT -u $USERNAME  --passwd-on-stdin $VPN_URL -s "$VPN_SLICE" &
+# shellcheck disable=SC1091
+source /opt/clip-connect/venv/bin/activate
+
+export LC_ALL="en_US.UTF-8"
+echo "$PASSWORD" | sudo "$OPENCONNECT" -u "$USERNAME"  --passwd-on-stdin "$VPN_URL" -s "$VPN_SLICE" &
 rc=$?
 pid=$!
 
 if [ $rc -eq 0 ]
 then
-   wait "$pid"
+   wait $pid
 else
    echo "openconnect return code $rc" 1>&2
 fi
---
+++ get.sh
@@ -0,0 +1,50 @@
+#!/bin/bash 
+#
+# Install clip-connect
+#
+# Dietrich Liko
+
+die() {
+    echo "$@" 1>&2
+    exit 1
+}
+
+# check python version
+python -c 'import sys; exit(1) if sys.version_info.major < 3 and sys.version_info.minor < 3 else exit(0)' || die "clip-connect requires python 3.3+"
+
+# find openconnect binary
+for path in "/opt/local/sbin/openconnect" "/usr/local/bin/openconnect"
+do
+    [ -e $path ] && OPENCONNECT="$path"
+done
+
+[ -z "$OPENCONNECT" ] && die "No opneconnect binary found." 
+
+TMPDIR="/tmp/clip-connect.$$"
+mkdir "$TMPDIR"
+
+curl -sL https://github.com/dietrichliko/clip-connect/tarball/main | tar xz -C "$TMPDIR"
+
+CLIP_CONNECT_DIR="/opt/clip-connect"
+sudo mkdir "$CLIP_CONNECT_DIR"
+sudo python -m venv "$CLIP_CONNECT_DIR/venv"
+
+# shellcheck disable=SC1091
+. "$CLIP_CONNECT_DIR/venv/bin/activate"
+
+sudo -H pip install --upgrade pip
+sudo -H pip install vpn-slice
+
+for name in "clip-connect.sh" "clip-credentials.sh" "uninstall.sh"
+do
+   sudo cp "$TMPDIR/$name" "$CLIP_CONNECT_DIR"
+done
+
+sudo cat - <<EOF > /etc/sudoers.d/openconnect
+%admin  ALL=(ALL) NOPASSWD: $OPENCONNECT
+EOF
+
+sudo cp "$TMPDIR/clip-connect.plist" ~/Library/LaunchAgents/clip-connect.plist
+
+rm -rf $TMPDIR
+launchctl load ~/Library/LaunchAgents/clip-connect.plist
